78aa82b3c67a77f3495415a348252a48de92c01f
Ephrem Sisay <ephisismu@gmail.com>
2021-09-02 05:40:18 -0700

    more

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HEADER_FILE
+#define HEADER_FILE
 
 int _putchar(char c);
 int _isupper(int c);
@@ -11,6 +11,7 @@ void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
+int main(void);
 void print_triangle(int size);
 void print_number(int n);
-#endif /* MAIN_H */
+#endif
