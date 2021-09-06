6e01f47c3c47ce56c01da03df52201c7a1d55760
Ephrem Sisay <ephisismu@gmail.com>
2021-09-06 07:23:15 -0700

    pointers

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -8,4 +8,10 @@ int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+
 #endif
