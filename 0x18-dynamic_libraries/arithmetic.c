613dedc6c40402b3d754063e30268acb80a52b4a
Ephrem Sisay <ephisismu@gmail.com>
2021-11-29 12:58:42 -0800

    crossover

---
+++ 0x18-dynamic_libraries/arithmetic.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "art.h"
+
+int add(int a, int b)
+{
+	int nadd = a+b;
+	return nadd;
+}
+int sub(int a, int b)
+{
+	int nsub = a-b;
+	return nsub;
+}
+int mul(int a, int b)
+{
+	int nmul = a*b;
+	return nmul;
+}
+int divi(int a, int b)
+{
+	int ndiv = a/b;
+	return ndiv;
+}
+int mod(int a, int b)
+{
+	int nmod = a%b;
+	return nmod;
+}
---
+++ 0x18-dynamic_libraries/art.h
@@ -0,0 +1,10 @@
+#ifndef ART_H
+#define ART_H
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int divi(int a, int b);
+int mod(int a, int b);
+
+#endif
